30685af66f116d6a9e65e15d8f70605eb9c9d1ad
vikiwarrior <vikrant3302@gmail.com>
2022-12-18 00:04:20 +0530

    home page illustration and animation

--- portfolio-website/src/app/home/home.component.ts
+++ portfolio-website/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild ,Renderer2 } from '@angular/core';
 import { ParallaxService } from '../services/parallax.service';
 
 @Component({
@@ -6,12 +6,39 @@ import { ParallaxService } from '../services/parallax.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
+
 export class HomeComponent {
   @ViewChild('elem') elem: ElementRef = {} as ElementRef;
+ @ViewChild('elem1') elem1: ElementRef = {} as ElementRef;
 
-  constructor(private parallaxService: ParallaxService) {}
+ mouseX = 0;
+ mouseY = 0;
+ top = 60;
+ left = 60;
+  constructor(private parallaxService: ParallaxService ,private renderer: Renderer2) {}
 
   ngAfterViewInit() {
     this.parallaxService.applyParallax(this.elem.nativeElement);
+    this.renderer.listen(
+      'document',
+      'mousemove',
+      (event: MouseEvent) => {
+        this.mouseX = event.clientX;
+        this.mouseY = event.clientY;
+        // console.log(this.mouseX);
+        
+        this.updatePosition();
+      }
+    );
+    // this.parallaxService.applyParallax1(this.elem1.nativeElement);
+    
+  }
+
+  updatePosition() {
+    this.elem1.nativeElement.style.top = this.mouseY + 'px';
+    // console.log(this.elem1.nativeElement.top);
+    this.elem1.nativeElement.style.left = this.mouseX + 'px';
   }
 }
--- portfolio-website/src/app/services/parallax.service.ts
+++ portfolio-website/src/app/services/parallax.service.ts
@@ -7,17 +7,19 @@ export class ParallaxService {
 
   constructor() { }
 
+
+  
   applyParallax(elem: HTMLElement) {
     // Add event listener
     window.addEventListener('mousemove', parallax);
 
     // Magic happens here
     function parallax(e :any) {
-        console.log(e)
+        // console.log(e)
       const _w = window.innerWidth / 2;
       const _h = window.innerHeight / 2;
       
-      console.log(elem.offsetHeight)
+      // console.log(elem.offsetHeight)
     //   const _w = 200 / 2;
     //   const _h = 200 / 2;
       
@@ -26,24 +28,48 @@ export class ParallaxService {
 //    }))
       const _mouseX = e.clientX;
       const _mouseY = e.clientY;
-      const _depth1 = `${80-(_mouseX - _w) * 0.001}% ${30-(_mouseY - _h) * 0.001}%`;
+      const _depth1 = `${70-(_mouseX - _w) * (0.01)/2}% ${30-(_mouseY - _h) * (0.04)/2}%`;
       const _depth2 = `${80-(_mouseX - _w) * 0.002}% ${30-(_mouseY - _h) * 0.002}%`;
       const _depth3 = `${(80-(_mouseX - _w) * 0.006)}% ${30-(_mouseY - _h) * 0.006}%`;
-      const x = `${_depth3}, ${_depth2}, ${_depth1}`;
-      console.log(x);
+      const x = `${_depth1}, ${_depth2}, ${_depth3}`;
+      // const x = `${_depth1}`;
+      // console.log(x);
       elem.style.backgroundPosition = x;
+   
     }
   }
 
-  applyParallax1(elem: HTMLElement, scrollTop: number, windowHeight: number) {
-    const elementTop = elem.offsetTop;
-    const elementHeight = elem.offsetHeight;
-    const viewportHeight = windowHeight;
+  applyParallax1(elem: HTMLElement) {
+    window.addEventListener('mousemove', parallax);
 
-    const scrollPercent = (scrollTop - elementTop) / (viewportHeight + elementHeight);
-    const parallax = -1 * scrollPercent * elementHeight;
+    // Magic happens here
+    function parallax(e :any) {
+        // console.log(e)
+      const _w = window.innerWidth / 2;
+      const _h = window.innerHeight / 2;
+      
+      // console.log(elem.offsetHeight)
+    //   const _w = 200 / 2;
+    //   const _h = 200 / 2;
+      
+//    console.log(elem.style.backgroundPosition.split(',').forEach(element => {
+//     element()  
+//    }))
+      const _mouseX = e.clientX;
+      const _mouseY = e.clientY;
+      const _depth1 = `${70-(_mouseX - _w) * (0.01)/2}% ${30-(_mouseY - _h) * (0.04)/2}%`;
+      const _depth2 = `${80-(_mouseX - _w) * 0.002}% ${30-(_mouseY - _h) * 0.002}%`;
+      const _depth3 = `${(80-(_mouseX - _w) * 0.006)}% ${30-(_mouseY - _h) * 0.006}%`;
+      const x = `${_depth1}, ${_depth2}, ${_depth3}`;
+      // const x = `${_depth1}`;
+      // console.log(x);
 
-    elem.style.transform = `translateY(${parallax}px)`;
-  }
+  //     top: calc(30% - 20px);
+  // left: calc(75% - 20px);
+      elem.style.left = `${90 +(_mouseX - _w) * (0.006)/2}%`;
+      elem.style.top = `${90+(_mouseY - _h) * (0.006)/2}%`;
+
+    }
+}
 }
 
